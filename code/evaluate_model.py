b64b9889b879073bb03d8034db998de177a28352
Liam Strilchuk <62297965+liamstrilchuk@users.noreply.github.com>
2026-04-10 22:27:07 -0400

    more writing

--- code/evaluate_model.py
+++ code/evaluate_model.py
@@ -3,7 +3,7 @@ from model import Model
 def evaluate_model(create_params={}, train_params={}):
 	model = Model()
 	model.create(**create_params)
-	_, time_taken = model.train(**train_params)
+	history, time_taken = model.train(**train_params)
 
 	report = model.get_report(output_dict=True)
-	return report, time_taken
+	return report, history, time_taken
